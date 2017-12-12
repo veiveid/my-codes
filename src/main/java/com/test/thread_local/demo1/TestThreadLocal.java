938313f49d2c59f5fe300633d90fd717eb21fc9a
weizhang240 <weizhang240@creditease.cn>
2017-12-12 10:15:07 +0800

    threadlocal

---
+++ src/main/java/com/test/thread_local/demo1/TestThreadLocal.java
@@ -0,0 +1,22 @@
+package com.test.thread_local.demo1;
+
+public class TestThreadLocal {
+
+    ThreadLocal<String> threadLocal = new ThreadLocal<>();
+
+    public void test1(){
+        String str = "123";
+        threadLocal.set(str);
+        str = null;
+        this.test2();
+    }
+
+    public void test2(){
+        System.out.println(threadLocal.get());
+    }
+
+    public static void main(String[] args) {
+        TestThreadLocal t = new TestThreadLocal();
+        t.test1();
+    }
+}
