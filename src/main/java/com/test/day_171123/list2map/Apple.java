bc3976dc5fcb73e22e3ebe48e850bada9c21b1ed
weizhang240 <weizhang240@creditease.cn>
2018-01-23 19:51:48 +0800

    list过滤、分组

---
+++ src/main/java/com/test/day_171123/list2map/Apple.java
@@ -0,0 +1,86 @@
+package com.test.day_171123.list2map;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Apple {
+
+    private Integer id;
+    private String name;
+    private BigDecimal money;
+    private Integer num;
+    public Apple(Integer id, String name, BigDecimal money, Integer num) {
+        this.id = id;
+        this.name = name;
+        this.money = money;
+        this.num = num;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getMoney() {
+        return money;
+    }
+
+    public void setMoney(BigDecimal money) {
+        this.money = money;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public static void main(String[] args) {
+        List<Apple> appleList = new ArrayList<>();//存放apple对象集合
+
+        Apple apple1 =  new Apple(1,"苹果1",new BigDecimal("3.25"),10);
+        Apple apple12 = new Apple(1,"苹果2",new BigDecimal("1.35"),20);
+        Apple apple2 =  new Apple(2,"香蕉",new BigDecimal("2.89"),30);
+        Apple apple3 =  new Apple(3,"荔枝",new BigDecimal("9.99"),40);
+
+        appleList.add(apple1);
+        appleList.add(apple12);
+        appleList.add(apple2);
+        appleList.add(apple3);
+
+        //分组
+        //List 以ID分组 Map<Integer,List<Apple>>
+        Map<Integer,List<Apple>> map = appleList.stream().collect(Collectors.groupingBy(Apple::getId));
+        System.out.println(map);
+
+        //过滤
+        //过滤出符合条件的数据
+        List<Apple> filterList = appleList.stream().filter(a -> a.getName().equals("香蕉")).collect(Collectors.toList());
+        System.out.println(filterList);
+
+
+        //将集合中的数据按照某个属性求和:
+        BigDecimal totalMoney = appleList.stream().map(Apple::getMoney).reduce(BigDecimal.ZERO,BigDecimal::add);
+        System.out.println(totalMoney);
+
+        //计算 数量
+        int sum = appleList.stream().mapToInt(Apple::getNum).sum();
+        System.out.println(sum);
+    }
+}
